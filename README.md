5474b4e5b0a569c27904abb2ead1173caa50bcd1
禺疆 <zhihao.gzh@alibaba-inc.com>
2015-01-13 17:15:08 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# amo
-A javascript library for creating CSS3 animation.
+## amo.js
+
+一个写css3动画的js库
+
+#### 光速使用
+
+##### 1. 引入amo.js，提供了cdn地址:
+
+    <script src="https://a.alipayobjects.com/u/localhost/js/201412/47giqj9WZp.js"></script>
+
+##### 2. 创建动画
+    
+    //创建css3动画
+    //第一个参数为起始值，第二个为默认值，都是css属性
+    var myAnim = Amo.createCssAnim({
+        'opacity': 0,
+        'top': '100px'
+    }, {
+        'opacity': 1,
+        'top': '200px'
+    });
+    
+    //你也可以创建多个动画帧
+    var myAnim = Amo.createCssAnim({
+        0: {
+            top: '20px'
+        },
+        20: {
+            top: '50px'
+        },
+        50: {
+            top: '80px'
+        },
+        70: {
+            top: '120px'
+        },
+        100: {
+            top: '900px'
+        }
+    });
+
+##### 3. 使用动画
+
+    //首先传入需要应用的节点，可以是原生节点，也可以jquery节点对象
+    myAnim.run(node, {
+        duration: 1000, //动画持续时间(ms)
+        easing: 'ease', //动画过渡动画，参看css3 animation 支持的动画easing
+        time: 1, //动画播放次数
+        delay: 1000, //动画延迟时间(ms)
+        direction: 'alternate' //播放完以后是否倒顺播放
+    }, function() {
+        //动画执行完的回调函数
+        console.log('over');
+    });
+    
+##### 就是这么简单...
---
+++ src/amo.js
@@ -0,0 +1,260 @@
+/**
+ * amo.js
+ * easy to use css animation
+ *
+ * @author niko
+ * @date 2014-12-31
+ */
+;
+(function (exports) {
+
+    var Animations = {};
+
+    //util function
+    function inArray(arr, item) {
+        for (var i = 0; i < arr.length; i++) {
+            if (item == arr[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    function isArray(arr) {
+        return Object.prototype.toString.call(arr) === '[object Array]' || arr.length !== undefined;
+    }
+
+    function isNumber(num) {
+        return Object.prototype.toString.call(num) === '[object Number]';
+    }
+
+    function addClass(node, clazz) {
+        var classArr = node.className.split(' ');
+        if (inArray(classArr, clazz) == -1) {
+            classArr.push(clazz);
+            node.className = classArr.join(' ');
+        }
+    }
+
+    function removeClass(node, clazz) {
+        var classArr = node.className.split(' ');
+        var index = inArray(classArr, clazz);
+        if (index >= 0) {
+            classArr.splice(index, 1);
+            node.className = classArr.join(' ');
+        }
+    }
+
+    //++++++++++++++++++++++
+    //The Sea.js plugin for embedding style text in JavaScript code
+    //https://github.com/seajs/seajs-style
+    //author @army8735
+    //++++++++++++++++++++++
+    var importStyle = (function importStyle() {
+
+        var RE_NON_WORD = /\W/g
+        var doc = document
+        var head = document.getElementsByTagName('head')[0] ||
+            document.documentElement
+        var styleNode;
+
+        var handle = function (cssText, id) {
+            if (id) {
+                // Convert id to valid string
+                id = id.replace(RE_NON_WORD, '-')
+
+                // Don't add multiple times
+                if (doc.getElementById(id)) return
+            }
+
+            var element;
+
+            // Don't share styleNode when id is spectied
+            if (!styleNode || id) {
+                element = doc.createElement('style')
+                id && (element.id = id)
+
+                // Adds to DOM first to avoid the css hack invalid
+                head.appendChild(element)
+            } else {
+                element = styleNode
+            }
+
+            // IE
+            if (element.styleSheet !== undefined) {
+
+                // http://support.microsoft.com/kb/262161
+                if (doc.getElementsByTagName('style').length > 31) {
+                    throw new Error('Exceed the maximal count of style tags in IE')
+                }
+
+                element.styleSheet.cssText += cssText
+            }
+            // W3C
+            else {
+                element.appendChild(doc.createTextNode(cssText))
+            }
+
+            if (!id) {
+                styleNode = element
+            }
+        }
+
+        return handle;
+
+    })();
+
+    function getRandomFrameName() {
+        return 'Amo-frame-' + Math.floor(Math.random() * 10000000);
+    }
+
+    function getRandomTName() {
+        return 'Amo-transition-' + Math.floor(Math.random() * 10000000);
+    }
+
+    function createAnimStyle(name, from, to) {
+
+        var keyframes = ['keyframes', '-webkit-keyframes', '-o-keyframes', '-moz-keyframes'];
+        var fullStyle = '';
+
+        var style = '{';
+
+        if (to) {
+            style += '0% {';
+            for (var prop in from) {
+                style += prop + ':';
+                style += from[prop] + ';';
+            }
+            style += '}';
+
+            style += '100% {';
+            for (var prop in to) {
+                style += prop + ':';
+                style += to[prop] + ';';
+            }
+            style += '}';
+        } else {
+            for (var prop in from) {
+                if (isNumber(prop * 1) && from.hasOwnProperty(prop)) {
+                    style += prop + '% {';
+                    for (var p in from[prop]) {
+                        style += p + ':';
+                        style += from[prop][p] + ';';
+                    }
+                    style += '}';
+                }
+            }
+        }
+
+        style += '}';
+
+        //兼容性
+        for (var i = 0; i < keyframes.length; i++) {
+            fullStyle += '@' + keyframes[i] + ' ' + name;
+            fullStyle += style;
+        }
+
+        return fullStyle;
+    }
+
+    function createCssTransition(animName, duration, easing, time, delay, direction, mode) {
+
+        var transitions = ['animation', '-webkit-animation', '-o-animation', '-moz-animatin'],
+            style = '';
+
+        var className = getRandomTName();
+
+        style += '.' + className + '{';
+        for (var i = transitions.length; i--;) {
+            style += transitions[i] + '-name:' + animName + ';';
+            style += transitions[i] + '-duration:' + (duration / 1000) + 's;';
+            style += transitions[i] + '-timing-function:' + easing + ';';
+            style += transitions[i] + '-iteration-count:' + time + ';';
+            style += transitions[i] + '-fill-mode:'+mode+';';
+            style += transitions[i] + '-delay:' + delay + ';';
+            style += transitions[i] + '-direction:' + direction + ';';
+        }
+        style += '}';
+
+        importStyle(style);
+
+        return className;
+    }
+
+    //create css keyframe
+    function keyframe(from, to) {
+
+        var frameName = getRandomFrameName();
+
+        Animations[frameName] = {
+            frameName: frameName,
+            style: createAnimStyle(frameName, from, to),
+            animate: animate
+        };
+
+        importStyle(Animations[frameName].style);
+
+        return Animations[frameName];
+    }
+
+    //create css animate class
+    function animate(conf) {
+
+        var time = conf.time || 1;
+        if (time < 0) {
+            time = 'infinite';
+        }
+
+        var duration = conf.duration || 1000,
+            easing = conf.easing || 'ease',
+            delay = conf.delay || 0,
+            direction = conf.direction || 'normal',
+            mode = conf.mode || 'forwards';
+
+        var className = createCssTransition(this.frameName, duration, easing, time, delay, direction, mode);
+
+        return {
+            className: className,
+            time: time,
+            duration: duration,
+            run: run
+        }
+    }
+
+    function run(node, callback) {
+
+        var className = this.className,
+            duration = this.duration,
+            time = this.time;
+
+        if (isArray(node)) {
+            Array.prototype.forEach.call(node, function (item) {
+                removeClass(item, className);
+                setTimeout(function() {
+                    addClass(item, className);
+                }, 0);
+            });
+        } else {
+            removeClass(node, className);
+            setTimeout(function() {
+                addClass(node, className);
+            }, 0);
+        }
+
+        if (isNumber(time)) {
+            setTimeout(function () {
+                //removeClass(node, className);
+                if (callback) {
+                    callback();
+                }
+            }, duration * time);
+        }
+    }
+
+    var Amo = {
+        keyframe: keyframe
+    };
+
+    exports.Amo = Amo;
+
+})(window);
